18da010a6023819b335ab93fbb6931e24bb93077
Ben888GitHub <benedictryan80@gmail.com>
2021-09-17 22:13:19 +0700

    add createBrowserHistory() function as props to Router

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const customHistory = createBrowserHistory();
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Router basename={`/project-shopping-tracker`}>
+		<Router basename={`/project-shopping-tracker`} history={customHistory}>
 			<App />
 		</Router>
 	</React.StrictMode>,
